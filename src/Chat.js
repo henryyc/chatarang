3690658cac3af40d73e7b90859222b00749b6fd9
Henry Chen <hyc@umd.edu>
2018-06-12 15:30:17 -0400

    adds hardcoded message

--- src/Chat.js
+++ src/Chat.js
@@ -5,12 +5,43 @@ import MessageList from './MessageList';
 import MessageForm from './MessageForm';
 
 class Chat extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [
+        {
+          id: 1,
+          userName: 'dstrus',
+          body: 'YEEEET ggx gang',
+        },
+        {
+          id: 2,
+          userName: 'dpalazzo',
+          body: 'f in the chat',
+        },
+      ],
+    };
+  }
+
+  addMessage = () => {
+    const messages = [...this.state.messages];
+    messages.push({
+      id: Date.now(),
+      userName: 'jieun',
+      body: 'i\'m excited for dinner',
+    });
+
+    // shorthand - key is same as variable name
+    this.setState({ messages });
+  }
+
   render() {
     return (
       <div className="Chat">
         <ChatHeader />
-        <MessageList />
-        <MessageForm />
+        <MessageList messages={this.state.messages} />
+        <MessageForm addMessage={this.addMessage}/>
       </div>
     );
   }
--- src/MessageList.js
+++ src/MessageList.js
@@ -2,24 +2,12 @@ import React from 'react';
 
 import Message from './Message';
 
-const MessageList = () => {
-  const messages = [
-    {
-      id: 1,
-      userName: 'dstrus',
-      body: 'YEEEET ggx gang',
-    },
-    {
-      id: 2,
-      userName: 'dpalazzo',
-      body: 'f in the chat',
-    },
-  ];
+const MessageList = (props) => {
 
   return (
     <div className="MessageList">
       {
-        messages.map((msg) => (
+        props.messages.map((msg) => (
           <Message key={msg.id} message={msg} />
         ))
       }
